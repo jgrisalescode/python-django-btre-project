9f006f0830224e3d6f84d77d35f40a3f010e3371
Julian Grisales <jgrisales@gmail.com>
2022-01-08 17:36:05 +0100

    Pages Templates & Base Layout

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, template_name='pages/index.html')
+
+
+def about(request):
+    return render(request, template_name='pages/about.html')
