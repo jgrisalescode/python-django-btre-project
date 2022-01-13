c25abc0dfe45d99bc23ac78dfd0fd50a8c5299c6
Julian Grisales <jgrisales@gmail.com>
2022-01-13 18:37:18 +0100

    Register & Login Templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import redirect, render
 
 def register(request):
-    return render(request, template_name='accounts/register.html')
+    if request.method == 'POST':
+        # Register user
+        pass
+    else:    
+        return render(request, template_name='accounts/register.html')
 
 def login(request):
-    return render(request, template_name='accounts/login.html')
+    if request.method == 'POST':
+        # Login user
+        pass
+    else:    
+        return render(request, template_name='accounts/login.html')
 
 def logout(request):
     return redirect('index')
