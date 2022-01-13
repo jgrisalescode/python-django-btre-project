d14ff94c9c9f87747e845ebcf7a8a8dabb72b257
Julian Grisales <jgrisales@gmail.com>
2022-01-13 15:10:05 +0100

    Search Form Choices

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
+
 from .models import Listing
+from listings.choices import bedroom_choices, price_choices, state_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -27,5 +29,10 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request, 'listings/search.html', context=context)
 
