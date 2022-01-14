bb0b1eb62784e0f06fc0e5e3c61cde2171f2cc25
Julian Grisales <jgrisales@gmail.com>
2022-01-14 12:22:34 +0100

    Contact Form Submission

--- contacts/admin.py
+++ contacts/admin.py
@@ -3,7 +3,7 @@ from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'listing', 'email', 'contact_date')
-    display_links = ('id', 'name')
+    list_display_links = ('id', 'name')
     search_fileds = ('name', 'email', 'listing')
     list_per_page = 25
     
