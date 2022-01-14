3401ec26bb19dfde002658e61cf5e0592eaaae22
Julian Grisales <jgrisales@gmail.com>
2022-01-14 10:19:09 +0100

    Contacts Admin Customization

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    display_links = ('id', 'name')
+    search_fileds = ('name', 'email', 'listing')
+    list_per_page = 25
+    
+
+admin.site.register(Contact, ContactAdmin)
